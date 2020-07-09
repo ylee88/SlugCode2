8cfdf765a3280b8124477c6d8d52c48df8fb2699
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-07-09 14:18:54 -0700

    add 2D MHD definitions

---
+++ src/2D/System/MHD/definition.h
@@ -0,0 +1,39 @@
+! common numerical constants
+#define PI 4.0 * ATAN(1.0)
+
+! multidimensional variables
+#define XDIM 1
+#define YDIM 2
+#define NDIM 2
+
+! primitive variables
+#define DENS_VAR 1
+#define VELX_VAR 2
+#define VELY_VAR 3
+#define VELZ_VAR 4
+#define MEGX_VAR 5
+#define MEGY_VAR 6
+#define MEGZ_VAR 7
+#define PRES_VAR 8
+#define NUMB_VAR 8
+
+! conservative variables
+#define MOMX_VAR 2
+#define MOMY_VAR 3
+#define MOMZ_VAR 4
+#define ENER_VAR 8
+#define NSYS_VAR 8
+
+! waves
+#define FAST_LEFT 1
+#define ALFVEN_LEFT 2
+#define SLOW_LEFT 3
+#define ENTROPY 4
+#define SLOW_RIGHT 5
+#define ALFVEN_RIGHT 6
+#define FAST_RIGHT 7
+#define DIVERGENCE 8
+#define NUMB_WAVE 8
+
+! setup parameters
+#define MAX_STRING_LENGTH 100
