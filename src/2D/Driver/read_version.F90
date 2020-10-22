c6128144840bd4ce01f582c841bd008d644db723
Youngjun Lee <youngjunlee7@gmail.com>
2020-10-23 02:16:06 +0900

    read GIT_VERSION in a correct path

--- src/2D/Driver/read_version.F90
+++ src/2D/Driver/read_version.F90
@@ -4,11 +4,17 @@ subroutine read_version(commit_hash)
 
   character(len=MAX_STRING_LENGTH), intent(IN OUT) :: commit_hash
 
-  integer :: uid
+  integer :: uid, pos
+  character(len=MAX_STRING_LENGTH) :: exec_cmd, path
 
   uid = 10
 
-  open(unit=uid, file='GIT_VERSION', status='unknown', action='read')
+  ! get exec path
+  call get_command_argument(0, exec_cmd)
+  pos = scan(exec_cmd, '/', .true.)
+  path = exec_cmd(1:pos)
+
+  open(unit=uid, file=TRIM(ADJUSTL(path))//'GIT_VERSION', status='unknown', action='read')
   read(uid, '(A)') commit_hash
   close(uid)
 
