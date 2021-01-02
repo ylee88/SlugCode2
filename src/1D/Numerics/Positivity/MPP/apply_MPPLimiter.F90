e86ec942454c5523487c30b2420aee87bd9bd789
Youngjun Lee <youngjunlee7@gmail.com>
2021-01-01 23:19:48 -0800

    Migrate MPP Limiter to 1D/3D. But not yet implemented

---
+++ src/1D/Numerics/Positivity/MPP/apply_MPPLimiter.F90
@@ -0,0 +1,26 @@
+subroutine apply_MPPLimiter(dt)
+
+#include "definition.h"
+
+  !!!!!!!!!!!!!!!!!!!!!!!
+  ! NOT YET IMPLEMENTED !
+  !!!!!!!!!!!!!!!!!!!!!!!
+
+end subroutine apply_MPPLimiter
+
+
+function get_pres(U) result(pres)
+
+  use sim_data,  only: sim_gamma
+
+  implicit none
+
+  real, dimension(NSYS_VAR), intent(IN) :: U
+  real :: ekin, eint, pres
+
+  ekin = 0.5*dot_product(U(MOMX_VAR:MOMX_VAR), U(MOMX_VAR:MOMX_VAR))/U(DENS_VAR)
+  eint = U(ENER_VAR) - ekin    ! eint = rho*e
+
+  pres = (sim_gamma-1.)*eint
+
+end function get_pres
